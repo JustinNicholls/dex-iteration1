a52d5275352df4add014ad5464e4e3265c0c76f1
Chun-Kwan Alex Fung <cfung58@uwo.ca>
2015-01-25 02:52:10 -0500

    Made changes to the accept-body.json and deliver-body.json with info that would work with Tumblr. Made changes to the delivery function in route_handler.js and abstract-channel-handler.js

--- routes/route_handler.js
+++ routes/route_handler.js
@@ -10,6 +10,19 @@ var _ = require('underscore');
 // internal modules
 var logger = require('../lib/util/logger');
 
+
+//Tumblr stuff
+var tumblr = require('tumblr.js');
+
+//Tumblr Client
+//create new client for tumblr with consumer_key, consumer_secret, token, token_secret
+  var client = tumblr.createClient({
+    consumer_key: 'G5DTquGeU2TaVZa2m91DGadxaIh3Yv7sxhT2D4QbW4lmys9Wck',
+    consumer_secret: '7hxOb0QDwJ65BbK2RWyvi9QbxOoxJ2T1UZYUzNGBsSjgtwRMLr',
+    token: '0UT2vXL9kN66aPBcHpZWXQyNz9gnjrnVVpQrVR7ANaMARofXVl',
+    token_secret: 's5J9fFYiEefBCqKn1ldOwHLiIAn2wAlo1tJJ5VK9zqXWCx0yR0'
+  });
+
 /**
  * Add routes for a third-party channel handler to an Express application.
  *
@@ -18,77 +31,78 @@ var logger = require('../lib/util/logger');
  */
 module.exports = function (app, handler) {
 
-    if (!handler) {
-        throw new Error('Service not found!');
-    }
-    
-    /*	
-    //testing tumblr user info 
-    app.get('/test', function (req, res) {
-    if (!tumblrOauthAccessToken || !tumblrOauthAccessTokenSecret) {
-        res.redirect('/auth/request');
-    }
+  if (!handler) {
+    throw new Error('Service not found!');
+  }
 
-    var client = tumblr.createClient({
-        consumer_key: 'G5DTquGeU2TaVZa2m91DGadxaIh3Yv7sxhT2D4QbW4lmys9Wck',
-        consumer_secret: '7hxOb0QDwJ65BbK2RWyvi9QbxOoxJ2T1UZYUzNGBsSjgtwRMLr',
-        token: '0UT2vXL9kN66aPBcHpZWXQyNz9gnjrnVVpQrVR7ANaMARofXVl',
-        token_secret: 's5J9fFYiEefBCqKn1ldOwHLiIAn2wAlo1tJJ5VK9zqXWCx0yR0'
+
+  //accept function
+  app.post('/accept', function (req, res) {
+    logger.debug("accept:", req.body);
+    var accepted = handler.accept(req.body);
+    //console.log("what is accepted? " + accepted);
+
+    //deliver post goes here if true
+    //NOTE: not sure if we really need to call deliver, or DEX calls deliver below
+
+    //end if
+
+
+    logger.debug("accepted:", accepted);
+    res.status(200).send({accepted: accepted});
+  });
+
+  app.post('/deliver', function(req, res, next) {
+    handler.deliver(req.body.params, req.body.playlist, function(err, result) {
+      if (err) {
+        console.log("NO SUCCESS :( " );
+        //console.log("the motherfucking error: " + err);
+        //console.log(JSON.stringify(err) ); //json stringify the err, which should show the text_options, image_options or video_options
+
+        //the code below doesn't work
+        //var text_options = JSON.stringify(err);
+        //console.log(text_options[2]);
+
+        //client.text('alexf388.tumblr.com', text_options, function (err2, data) {
+          //if you uncomment this line, error: can't set headers after they are sent.
+          //res.send(data);
+
+        //});
+
+
+        next(err);
+
+      } else {
+        console.log("GREAT SUCCESS :) ");
+        logger.debug("delivered:", result);
+
+
+        res.status(200).send(result);
+      }
     });
+  });
 
-    client.userInfo(function (err, data) {
-        res.send(data);
+  app.post('/feedback', function(req, res, next) {
+    handler.getFeedback(req.body, function(err, feedback) {
+      if (err) {
+        next(err);
+      } else {
+        logger.debug("feedback:", feedback);
+        res.status(200).send(feedback);
+      }
     });
+  });
+
+  app.post('/remove', function(req, res, next) {
+    logger.debug("remove: ", req.body);
+    handler.remove(req.body, function(err) {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).send();
+      }
     });
-    */
+  });
 
-
-    app.post('/accept', function (req, res) {
-        logger.debug("accept:", req.body); 
-        var accepted = handler.accept(req.body);
-
-	//deliver post goes here if true 
-	//NOTE: not sure if we really need to call deliver, or DEX calls deliver below 
-	
-	//end if 	
-
-
-        logger.debug("accepted:", accepted);
-        res.status(200).send({accepted: accepted});
-    });
-
-    app.post('/deliver', function(req, res, next) {
-        handler.deliver(req.body.params, req.body.playlist, function(err, result) {
-            if (err) {
-                next(err);
-            } else {
-                logger.debug("delivered:", result);
-                res.status(200).send(result);
-            }
-        });
-    });
-
-    app.post('/feedback', function(req, res, next) {
-        handler.getFeedback(req.body, function(err, feedback) {
-            if (err) {
-                next(err);
-            } else {
-                logger.debug("feedback:", feedback);
-                res.status(200).send(feedback);
-            }
-        });
-    });
-
-    app.post('/remove', function(req, res, next) {
-        logger.debug("remove: ", req.body);
-        handler.remove(req.body, function(err) {
-            if (err) {
-                next(err);
-            } else {
-                res.status(200).send();
-            }
-        });
-    });
-
-    return app;
+  return app;
 };
